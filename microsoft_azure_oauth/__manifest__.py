950a9aa639862e90f9fecd59f895b81b4e78f706
Ali Husnain Arshad <banti6350@gmail.com>
2022-01-28 17:06:56 +0500

    Add files via upload

---
+++ microsoft_azure_oauth/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Microsoft Azure AD OAuth",
+    "version": "1.0",
+    'category': 'Hidden/Tools',
+    "license": "AGPL-3",
+    "author": "Ali Husnain Arshad",
+    "website": "https://boltflare.com/odoo-development/",
+    'price': 0.00,
+    'currency': 'USD',
+    "summary": "Allow users to login using Azure Active Directory (Azure AD).",
+    "depends": ["auth_oauth"],
+    'data': [
+        'data/auth_oauth_data.xml',
+    ],
+    'images': ['static/description/icon.png'],
+
+}
